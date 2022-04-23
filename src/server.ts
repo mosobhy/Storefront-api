3ac517216c340786e6aa797a66a713185dd513ed
mosobhy <mosobhy266@gmail.com>
2022-04-23 02:51:35 +0200

    settings added

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,10 @@ const address: string = "0.0.0.0:3000"
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
+app.listen(3000, () => {
+    console.log(`starting app on: http://localhost:${address}`)
 })
